1eb401822a58f04f781bc3c0e1fac6fb1270bd22
Andrew Pinon <andrewpinon@192.168.254.109>
2023-05-01 22:00:28 +0800

    remove lint errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
-export default function Document() {
+const Document: any = () => {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
+
+export default Document
