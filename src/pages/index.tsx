e4ffc359022d93371edda965879e89d94ac54924
Andrew Pinon <andrewpinon@192.168.254.109>
2023-05-03 04:35:49 +0800

    configure azure depoloyment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Home: React.FC = () => {
-  return <div className='h-screen w-full'>test 3</div>
+  return <div className='h-screen w-full'>test 4</div>
 }
 
 export default Home
