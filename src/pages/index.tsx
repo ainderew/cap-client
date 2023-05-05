0c3643d8278abf7076f4bfdbd2d35c3a75e43cfd
sunias17 <brettjosef.galvez@cit.edu>
2023-05-05 13:58:35 +0800

    5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Home: React.FC = () => {
-  return <div className='h-screen w-full'>test 4</div>
+  return <div className='h-screen w-full'>test 5</div>
 }
 
 export default Home
