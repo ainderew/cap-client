508831ba3fb9419ee3f58a09c72d20e97239930c
market0v0 <limanafive@gmail.com>
2023-09-20 22:36:27 +0800

    temp

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,57 +3,7 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const NavBar: React.FC = () => {
-  const router = useRouter()
-  const handleRedirect = (route: string): void => {
-    router.push(route).catch(err => {
-      throw err
-    })
-  }
-
-  return (
-    <div className='color-white hidden h-16 w-full items-center justify-between px-28 lg:flex'>
-      <div className='logo-container flex h-full items-center gap-2'>
-        <div className='relative h-full w-7'>
-          <Image src={'/logo.svg'} fill alt='chatxpert' />
-        </div>
-        <span className='font-semibold'>ChatXpert</span>
-      </div>
-      {/* <ul className="flex flex-1">
-    <div className='color-white hidden h-24 w-full lg:flex'>
-      <div className='w-full h-full mx-48 flex items-center justify-between border-b-2'>
-        <div className='logo-container flex h-full items-center gap-2'>
-          <div className='relative h-full w-7'>
-            <Image src={'/logo.svg'} fill alt='chatxpert' />
-          </div>
-          <span className='font-semibold'>ChatXpert</span>
-        </div>
-        {/* <ul className="flex flex-1">
-        <li className="">Test</li>
-        <li className="">Test</li>
-        <li className="">Test</li>
-        <li className="">Test</li>
-      </ul> */}
-
-      <div className='flex gap-4'>
-        <button
-          onClick={() => {
-            handleRedirect('/register/customer')
-          }}
-          className=''
-        >
-          Sign Up
-        </button>
-        <button
-          onClick={() => {
-            handleRedirect('/login')
-          }}
-          className=''
-        >
-          Login
-        </button>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
 
 export default NavBar
