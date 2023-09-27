ea10c5ee361325b33690d0c650a4f5035fa58e3b
market0v0 <limanafive@gmail.com>
2023-09-27 23:51:26 +0800

    1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,30 +12,14 @@ const config: ThemeConfig = {
   // algorithm: theme.darkAlgorithm
   // Add theme here on free time - %%obsidian remind
 }
-<<<<<<< HEAD
-
-/* const ALLOWED_URL = ['/', '/login'] */
-=======
 // this should be changed cause this might be wrong hahahahahaha
 const ALLOWED_URL = ['/', '/login', '/register/customer', '/register/business', '/business/data-management']
 const CUSTOMER_ALLOWED_URL = ['/', '/home']
 const BUSINESS_ALLOWED_URL = ['/', '/business/dashboard', '/business/data-management']
->>>>>>> galvez
 
 const App: any = ({ Component, pageProps }: AppProps) => {
   const router = useRouter()
 
-<<<<<<< HEAD
-/*   useEffect(() => {
-    const currentUrl = router.asPath
-
-    const allow = ALLOWED_URL.find(link => link === currentUrl)
-    if (allow !== undefined) return // add check if user is logged in then disable redirect
-    router.replace('/login').catch(err => {
-      throw err
-    })
-  }, []) */
-=======
   const { authStore } = useStores()
 
   const userType = authStore.userProfile?.profile.type
@@ -65,7 +49,6 @@ const App: any = ({ Component, pageProps }: AppProps) => {
       })
     }
   }, [])
->>>>>>> galvez
   return (
     <ConfigProvider theme={config}>
       <DefaultLayout>
