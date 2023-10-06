b674e2c2d494a6c07d0f3f77ca63584b35c7a1c3
Andrew Pinon <andrewpinon@192.168.254.107>
2023-10-06 20:49:49 +0800

    fix send upload data to backend

---
+++ src/hooks/usePostData.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import { useStores } from '@/core/stores/UseStores'
+
+interface fetchStateTypes {
+  loading: boolean
+  data: unknown
+}
+
+type Filter = Record<string, unknown>
+
+export default function usePostData (endpoint: string, variables: Filter): fetchStateTypes {
+  const [fetchStates, setFetchedStates] = useState<fetchStateTypes>({
+    loading: false,
+    data: null
+  })
+
+  const { authStore: { userProfile } } = useStores()
+  const accountId = userProfile?.profile._id
+
+  const filter: Filter = variables ?? {}
+
+  useEffect(() => {
+    setFetchedStates(prev => {
+      return {
+        ...prev,
+        loading: true
+      }
+    })
+
+    if (accountId !== null) {
+      filter.accountId = accountId
+    }
+  }, [])
+
+  fetch(endpoint, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(filter)
+  })
+    .then(async res => await res.json())
+    .then(data => {
+      setFetchedStates({
+        loading: false,
+        data
+      })
+    })
+    .catch(err => {
+      throw err
+    })
+
+  return fetchStates
+}
