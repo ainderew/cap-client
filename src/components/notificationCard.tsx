2f2b0342090ce5517faf5d0b3e3db6a7ae369f50
market0v0 <limanafive@gmail.com>
2023-10-11 01:14:38 +0800

    notification redesign

--- src/components/notificationCard.tsx
+++ src/components/notificationCard.tsx
@@ -24,20 +24,22 @@ const calculateDateDisplay = (date: string): string => {
   }
 }
 
-const NotificationCard: React.FC<Data> = props => {
+const NotificationCard: React.FC<Data> = (props) => {
   const formattedDate = calculateDateDisplay(props.date)
 
   return (
-    <div className='grid grid-cols-4 rounded-md bg-white px-2 py-2'>
-      <div className='flex justify-center rounded-full border-2 border-solid border-[#96969616] bg-[#fff] shadow-md'>
-        <div className='relative h-full  w-10 '>
+    <div className='grid min-h-[7rem] grid-cols-4  rounded-md bg-white py-2 pl-2  drop-shadow-md'>
+      <div className='flex items-center   justify-center  rounded-full border-2 border-solid border-[#96969616]'>
+        <div className='relative h-10 w-8 '>
           <Image src={'/logo.svg'} fill alt='chatxpert' />
         </div>
       </div>
-      <div className='col-span-3 '>
-        <h1 className='pb-2 text-[1rem] font-semibold'>{props.title}</h1>
-        <p className='pl-2 text-[0.9]'>{props.message}</p>
-        <h1 className='pb-2 text-[1rem] font-semibold text-[#2B99FF]'>{formattedDate}</h1>
+      <div className='col-span-3 px-2 py-2 '>
+        <h1 className='text-[.8rem] font-semibold'>{props.title}</h1>
+        <p className=' text-[0.7rem]'>{props.message}</p>
+        <h1 className=' text-[.7rem] font-semibold text-[#2B99FF]'>
+          {formattedDate}
+        </h1>
       </div>
     </div>
   )
