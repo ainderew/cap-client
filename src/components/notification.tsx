2c49ccc3eaf898010fedbdb71bbda8b8a3b0c5cb
market0v0 <limanafive@gmail.com>
2023-10-11 01:22:12 +0800

    notification

--- src/components/notification.tsx
+++ src/components/notification.tsx
@@ -33,16 +33,6 @@ const NotificationBar: React.FC = () => {
       date: '7/2/2023',
       message: 'Your data is not up to date it needs to be updated',
     },
-    {
-      title: 'DATA UPDATE',
-      date: '7/2/2023',
-      message: 'Your data is not up to date it needs to be updated',
-    },
-    {
-      title: 'DATA UPDATE',
-      date: '7/2/2023',
-      message: 'Your data is not up to date it needs to be updated',
-    },
   ]
 
   return (
@@ -50,7 +40,7 @@ const NotificationBar: React.FC = () => {
       <span className='sticky top-0 z-10 flex items-center justify-start   bg-[#ffffff] px-2 py-2 text-[1.3rem]  '>
         Notification
       </span>
-      <section className='z-0 max-h-[45rem] overflow-auto '>
+      <section className='z-0 max-h-[70vh]  overflow-auto '>
         {datas.length === 0 ? (
           <div className='flex justify-center py-4'>No Notifications</div>
         ) : (
