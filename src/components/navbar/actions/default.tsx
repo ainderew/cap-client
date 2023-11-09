09bb6efd013e410cb99abb8e2bd2a41b06a6fe72
Andrew Pinon <andrewpinon@192.168.254.107>
2023-11-09 21:42:25 +0800

    improved registration

--- src/components/navbar/actions/default.tsx
+++ src/components/navbar/actions/default.tsx
@@ -8,7 +8,7 @@ function DefaultNavActions(){
     return(
       <>
         <Button onClick={() => { void router.push('/login') }} className='bg-none border-none shadow-none'>Login</Button>
-        <Button onClick={() => { void router.push('/register/customer') }} className='bg-primary border-none text-white hover:!text-white hover:bg-secondary hover:border-none'>Sign Up</Button>
+        <Button onClick={() => { void router.push('/register/business') }} className='bg-primary border-none text-white hover:!text-white hover:bg-secondary hover:border-none'>Sign Up</Button>
       </>
     )
 }
